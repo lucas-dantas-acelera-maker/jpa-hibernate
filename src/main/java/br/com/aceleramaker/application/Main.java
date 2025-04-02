b34525807f24846e73470f89d164377c12bade59
ro77en <aluno17@aceleramaker.com.br>
2025-04-02 12:53:32 -0300

    feat: add TypedQuery to get all User objects, setting limit of results

--- src/main/java/br/com/aceleramaker/application/Main.java
+++ src/main/java/br/com/aceleramaker/application/Main.java
@@ -3,6 +3,9 @@ package br.com.aceleramaker.application;
 import br.com.aceleramaker.model.User;
 import br.com.aceleramaker.util.JPAUtil;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,9 +22,22 @@ public class Main {
         * */
 
         // Get User By Identifier
-        // em.find(Class to be found, Identifier) -> returns an Object (User, in this case)
-        User foundUser = em.find(User.class, 7L);
-        System.out.println(foundUser.getId() + " - " + foundUser.getName() + " - " + foundUser.getEmail());
+        /*
+        * em.find(Class to be found, Identifier) -> returns an Object (User, in this case)
+        * User foundUser = em.find(User.class, 7L);
+        * System.out.println(foundUser.getId() + " - " + foundUser.getName() + " - " + foundUser.getEmail());
+        * */
+
+        // Get All Users
+        /*
+        * TypedQuery<ReturnedClass> query = em.createQuery(query, returned class)
+        * */
+        String jpql = "SELECT u from User u";
+        TypedQuery<User> query = em.createQuery(jpql, User.class);
+        query.setMaxResults(5);
+        List<User> users = query.getResultList();
+
+        users.forEach(u -> System.out.printf("%d - %s - %s%n", u.getId(), u.getName(), u.getEmail()));
 
         em.close();
     }
