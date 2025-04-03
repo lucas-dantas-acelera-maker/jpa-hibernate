4af9f6fe56951e9b0169471d419a1e4c526d9a79
ro77en <aluno17@aceleramaker.com.br>
2025-04-03 11:43:09 -0300

    feat: add toString method for model classes

--- src/main/java/br/com/aceleramaker/model/Product.java
+++ src/main/java/br/com/aceleramaker/model/Product.java
@@ -42,4 +42,9 @@ public class Product {
     public void setPrice(Double price) {
         this.price = price;
     }
+
+    @Override
+    public String toString() {
+        return  id + " - " + name + " $ " + price;
+    }
 }
--- src/main/java/br/com/aceleramaker/model/User.java
+++ src/main/java/br/com/aceleramaker/model/User.java
@@ -40,4 +40,9 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " - " + email;
+    }
 }
