b8de830d0daba175f047200580ce031e3e59fe33
ro77en <aluno17@aceleramaker.com.br>
2025-04-03 10:23:57 -0300

    feat: add new Product using DAO

--- src/main/java/br/com/aceleramaker/application/Main.java
+++ src/main/java/br/com/aceleramaker/application/Main.java
@@ -1,6 +1,13 @@
 package br.com.aceleramaker.application;
 
+import br.com.aceleramaker.model.Product;
+import br.com.aceleramaker.repository.DAO;
+
 public class Main {
     public static void main(String[] args) {
+        Product p = new Product("Pen", 7.45);
+
+        DAO< Product> productDAO = new DAO<>(Product.class);
+        productDAO.addEntity(p).close();
     }
 }
