4fbc3f611d32e8bcd2eaecfe1c0e4d64363dcabe
ro77en <aluno17@aceleramaker.com.br>
2025-04-03 11:44:30 -0300

    feat: add menu to handle different entities operations

--- src/main/java/br/com/aceleramaker/application/Main.java
+++ src/main/java/br/com/aceleramaker/application/Main.java
@@ -1,13 +1,72 @@
 package br.com.aceleramaker.application;
 
 import br.com.aceleramaker.model.Product;
+import br.com.aceleramaker.model.User;
 import br.com.aceleramaker.repository.DAO;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Product p = new Product("Pen", 7.45);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("=========JPA HIBERNATE=========");
+            System.out.println("1 - User related operations");
+            System.out.println("2 - Product related operations");
+            String userIn = sc.nextLine();
 
-        DAO< Product> productDAO = new DAO<>(Product.class);
-        productDAO.addEntity(p).close();
+            DAO<?> dao;
+            switch (userIn) {
+                case "1" -> dao = new DAO<>(User.class);
+                case "2" -> dao = new DAO<>(Product.class);
+                default -> throw new IllegalArgumentException("Please select a valid option.");
+            }
+
+            String entityName = dao.getEntityClass().getSimpleName();
+
+            System.out.println("=========SELECT OPERATION=========");
+            System.out.printf("1 - %s: Add new%n", entityName);
+            System.out.printf("2 - %s: List all%n", entityName);
+            userIn = sc.nextLine();
+
+            Field[] fields = dao.getEntityClass().getDeclaredFields();
+
+            String entityAttr1 = fields[1].getName();
+            String entityAttr2 = fields[2].getName();
+
+            switch (userIn) {
+                case "1":
+                    System.out.printf("%s %s: ", entityName, entityAttr1);
+                    String name = sc.nextLine();
+                    System.out.printf("%s %s: ", entityName, entityAttr2);
+                    String in = sc.nextLine();
+
+                    if (fields[2].getType() == Double.class) {
+                        @SuppressWarnings("unchecked")
+                        DAO<Product> productDAO = (DAO<Product>) dao;
+
+                        double price = Double.parseDouble(in);
+                        Product e = new Product(name, price);
+                        productDAO.addEntity(e).close();
+                    } else {
+                        @SuppressWarnings("unchecked")
+                        DAO<User> userDAO = (DAO<User>) dao;
+
+                        User e = new User(name, in);
+                        userDAO.addEntity(e).close();
+                    }
+                    break;
+                case "2":
+                    List<?> entities = dao.getEntities();
+                    entities.forEach(System.out::println);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Please select a valid option.");
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
